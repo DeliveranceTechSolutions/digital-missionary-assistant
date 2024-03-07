a2db06d1664a1d2718b3a590fb8fc7c517109260
DeliveranceTechSolutions <kirklincoln@gmail.com>
2024-03-06 16:36:30 -0800

    took out something that might be sensitive

--- app/classifier/constants.py
+++ app/classifier/constants.py
@@ -23,38 +23,3 @@ INBOX_OWNER_EMAIL = "Inspiracion@carypalmon.org"
 MESSAGE_ID = 'MessageID'
 UNKNOWN_LABEL = "Unknown Label"
 LIST_OF_LABELS = ['2 Book Request','3 Prayer Request','5 Various','1 No Response Needed']
-SIGNATURES_TO_REMOVE_AFTER = [
-    '-------- Mensaje original --------',
-    'Enviado desde Yahoo',
-    '<inspiracion+carypalmon.org@ccsend.com>',
-    INBOX_OWNER_EMAIL,
-    "<\n" + INBOX_OWNER_EMAIL + "\n>",
-    'Inspiracion Cary Palmon Ministries (',
-    'reaccionó a través de\nGmail',
-    'Cary Palmón Ministries (',
-    ', Cary Palmón Ministries',
-    'Enviado desde mi',
-    'Obtener\nOutlook para Android',
-    'De:\nCary',
-    'De:\n<',
-    ', Cary Palmon Ministries',
-    'Outlook para Android',
-    'Sent from my iPhone',
-    '" Send For Moto g 8 Power"',
-    '---------- Forwarded message ---------',
-    'Enviado desde HUAWEI',
-    'Libre de virus',
-    'Enviado desde\nOutlook',
-    'Sent from my MetroPCS'
-]
-
-
-# For removing outlier data
-MESSAGE_IDS_TO_REMOVE = [
-    # '<CALbQTR2URxjCmE7tuVC+maV7R8n1XM9D3OuTU5BE2erHvYOxjg@mail.gmail.com>',
-    # '<CALbQTR3BT3rc8_3wAE+WuDXJ0UgX4Hc8_DQajoC2sENQXvAvDg@mail.gmail.com>',
-    # '<CAJDEu3Oi=ZbJaLjPTqMMTOcRM3_Jm1inSFk8wbE2UJmdGFzOWg@mail.gmail.com>',
-    # '<CAO8B5JbgrZasAMNV4vLaPm6xsoKV_Sr5pF1-D80Vkb=WQ76CCQ@mail.gmail.com>',
-    # '<CANXk2nYDOOxWdvDbYgQv4N9-5A8KB+XukCNemcmOCV4_D1kisA@mail.gmail.com>',
-    # '<CAOAbZTwGnHZRWvO9cwxEOpNTbdzTS0dho=nUy8rCkKnsbed8Rg@mail.gmail.com>',
-]
